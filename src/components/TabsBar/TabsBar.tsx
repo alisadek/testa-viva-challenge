62b5d26b4621537039971afefd36926a05d1495e
Ali Sadek <ali.sadek@qpr.com>
2023-02-13 03:10:52 +0200

    feature: add logic to tabsbar

--- src/components/TabsBar/TabsBar.tsx
+++ src/components/TabsBar/TabsBar.tsx
@@ -1,16 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './TabsBar.module.scss';
 import logo from '../../assets/icon-logo.png';
+import { getDocuments } from '../../api/apis';
 
-type Props = {};
+type Tab = { id: number; title: string };
+type Props = { tabs: Tab[]; initialSelection?: number; onChange: (selection: number) => void };
 
 const TabsBar = (props: Props) => {
+  const [selected, setSelected] = useState<number>(props.initialSelection || 1);
+  const { tabs } = props;
   return (
     <div className={styles.bar}>
       <div className={styles.buttonsGroup}>
-        <button className={styles.tab}>Dokument 1</button>
-        <button className={`${styles.tab} ${styles.selected}`}>Dokument 2</button>
-        <button className={styles.tab}>Dokument 3</button>
+        {tabs.map(tab => (
+          <button
+            key={tab.id}
+            className={`${styles.tab}  ${tab.id === selected ? styles.selected : ''}`}
+            onClick={() => {
+              setSelected(tab.id);
+              props.onChange(tab.id);
+            }}
+          >
+            {tab.title}
+          </button>
+        ))}
       </div>
       <img className={styles.logo} src={logo} alt="logo" />
     </div>
