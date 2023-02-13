467efadc97d68f641a7163cc916c554f0d643cd2
Ali Sadek <ali.sadek@qpr.com>
2023-02-13 16:48:34 +0200

    feature: scroll logic. new comment. click outside.

--- src/components/CommentsBar/CommentsBar.tsx
+++ src/components/CommentsBar/CommentsBar.tsx
@@ -1,45 +1,114 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useRef } from 'react';
 import styles from './CommentsBar.module.scss';
 import commentIcon from '../../assets/icon-comment.png';
 import trashIcon from '../../assets/icon-trash.png';
 import addIcon from '../../assets/icon-add.png';
 import arrowIcon from '../../assets/icon-arrow.png';
+import { Comment } from '../../types';
+import useOutsideClick from '../../hooks/useOnClickOutside';
 
-type Props = {};
+type Props = {
+  onInputChange: (event: React.ChangeEvent<HTMLTextAreaElement>) => void;
+  comments?: Partial<Comment>[] | null;
+  onSubmit: () => void;
+  onSelectComment: (comment?: Partial<Comment>) => void;
+  activeComment: Partial<Comment> | null;
+  onAddComment: () => void;
+};
 
 const CommentsBar = (props: Props) => {
   const [open, toggleOpen] = useState(false);
-  useEffect(() => {
-    console.log('IsOpen:', open);
-  }, [open]);
+  const [scrollBoundaries, setScrollBoundaries] = useState<{ start: number; end: number }>({
+    start: 0,
+    end: 4
+  });
 
+  const ref = useRef();
+  useOutsideClick(ref, () => {
+    toggleOpen(false);
+  });
+
+  const { onInputChange, comments, onSubmit } = props;
+  const ACTIVE_COMMENTS = comments?.slice(scrollBoundaries.start, scrollBoundaries.end);
+  const handleScrollDown = () => {
+    if (comments && comments.length > 4) {
+      if (
+        scrollBoundaries.end <= comments.length &&
+        scrollBoundaries.end - scrollBoundaries.start === 4
+      ) {
+        setScrollBoundaries(prevBoundaries => ({
+          end: prevBoundaries.end + 1,
+          start: prevBoundaries.start + 1
+        }));
+      }
+    }
+  };
+  const handleScrollUp = () => {
+    if (comments && comments.length > 4) {
+      if (scrollBoundaries.end > 4 && scrollBoundaries.start > 0) {
+        setScrollBoundaries(prevBoundaries => ({
+          end: prevBoundaries.end--,
+          start: prevBoundaries.start--
+        }));
+      }
+    }
+  };
   return (
-    <div className={`${styles.bar} ${open && styles.isOpen}`}>
+    <div className={`${styles.bar} ${open && styles.isOpen}`} ref={ref as any}>
       {open && (
         <div className={styles.commentSection}>
-          <textarea placeholder="Enter your comment..." className={styles.textArea} />
+          <textarea
+            onChange={onInputChange}
+            placeholder="Enter your comment..."
+            className={styles.textArea}
+            value={props.activeComment?.comment}
+          />
           <div className={styles.actionsSection}>
-            <button>Gem</button>
+            <button onClick={onSubmit}>Gem</button>
             <img src={trashIcon} alt="trash icon" />
           </div>
         </div>
       )}
       <div className={styles.iconsColumn}>
         <div className={styles.upperIcons}>
-          <img className={`${styles.icon} ${styles.arrowUp}`} src={arrowIcon} alt="arrow up icon" />
-          <div className={styles.commentIcons}>
-            <img
-              onClick={() => toggleOpen(!open)}
-              className={styles.commentIcon}
-              src={commentIcon}
-              alt="Comment Icon"
-            />
-            <img className={styles.commentIcon} src={commentIcon} alt="Comment Icon" />
-            <img className={styles.commentIcon} src={commentIcon} alt="Comment Icon" />
-          </div>
-          <img className={styles.icon} src={addIcon} alt="Plus Icon" />
+          <img
+            className={`${styles.icon} ${styles.arrowUp}`}
+            src={arrowIcon}
+            alt="arrow up icon"
+            onClick={handleScrollUp}
+          />
+          {!!comments?.length && (
+            <div className={styles.commentIcons}>
+              {ACTIVE_COMMENTS?.map(comment => (
+                <img
+                  onClick={() => {
+                    toggleOpen(!open);
+                    props.onSelectComment(comment);
+                  }}
+                  className={styles.commentIcon}
+                  src={commentIcon}
+                  key={comment.id}
+                  alt="Comment Icon"
+                />
+              ))}
+            </div>
+          )}
+          <img
+            className={styles.icon}
+            src={addIcon}
+            alt="Plus Icon"
+            onClick={() => {
+              toggleOpen(!open);
+              props.onAddComment();
+            }}
+          />
         </div>
-        <img className={styles.icon} src={arrowIcon} alt="Plus Icon" />
+        <img
+          className={styles.icon}
+          src={arrowIcon}
+          alt="arrow down Icon"
+          onClick={handleScrollDown}
+        />
       </div>
     </div>
   );
