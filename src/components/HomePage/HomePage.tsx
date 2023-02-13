3c6fdb8281f543dd7c268b84da1e93e34bb224ee
Ali Sadek <ali.sadek@qpr.com>
2023-02-13 09:25:02 +0200

    feature: integrate documents api

--- src/components/HomePage/HomePage.tsx
+++ src/components/HomePage/HomePage.tsx
@@ -1,4 +1,7 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useDocuments } from '../../hooks/documents.hook';
+import { Document } from '../../types';
+import axios from 'axios';
 import Card from '../Card/Card';
 import ParagraphCard from '../ParagraphCard/ParagraphCard';
 import TabsBar from '../TabsBar/TabsBar';
@@ -7,23 +10,33 @@ import styles from './HomePage.module.scss';
 type Props = {};
 
 const HomePage = (props: Props) => {
+  const { data: documents, isLoading, error } = useDocuments();
+  const [activeDocument, setActiveDocument] = useState<Document | null | undefined>(null);
+
+  useEffect(() => {
+    setActiveDocument(documents?.['hydra:member']?.[0]);
+  }, [documents]);
+
+  if (isLoading) return <h1>Loading...</h1>;
+  if (error && axios.isAxiosError(error))
+    return <h3 style={{ color: 'red' }}>An error has occurred: {error.message}</h3>;
+
+  const handleChange = (selectedId: number) => {
+    const currentDocument = documents?.['hydra:member'].find(doc => doc.id === selectedId);
+    if (currentDocument) setActiveDocument(currentDocument);
+  };
+
   return (
     <div className={styles.page}>
-      <TabsBar />
+      <TabsBar
+        onChange={handleChange}
+        tabs={documents?.['hydra:member'].map(doc => ({ id: doc.id, title: doc.title })) || []}
+      />
       <div className={styles.documentViewer}>
         <Card>
-          <h1>Dokument</h1>
+          <h1>{activeDocument?.title}</h1>
           <ParagraphCard>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. At enim nec enim, at arcu
-              scelerisque. Orci, mi mattis non auctor. Vel, risus nisl, lobortis venenatis, arcu
-              donec metus sit euismod. Venenatis molestie volutpat nunc amet eget quis velit
-              ultrices ac. Lectus semper ut odio ante tempor nunc tempor id. Imperdiet nisi aliquam
-              leo, vitae tellus condimentum mauris, aliquet. Vestibulum, id nullam scelerisque sit
-              ipsum at porta. Ut adipiscing tellus gravida sodales cursus ut rutrum. Gravida
-              adipiscing elementum, amet dui vulputate maecenas imperdiet lacus. Facilisis eget
-              tincidunt sem id et urna quisque.
-            </p>
+            <p>{activeDocument?.content}</p>
           </ParagraphCard>
         </Card>
       </div>
